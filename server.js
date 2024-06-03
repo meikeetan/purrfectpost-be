e61bf546ff0ef5afbeb86cbb7243e72354035e3f
Tan Mei Kee <meikee@Tans-MacBook-Air.local>
2024-06-03 22:17:11 +0800

    added routes, controllers, middleware

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 8000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.get('/', (req, res) => {
     res.send('Hello')
 })
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
