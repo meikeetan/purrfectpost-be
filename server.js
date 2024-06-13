41870c704c618cc29a401ae5211b6417582351c6
Tan Mei Kee <meikee@Tans-MacBook-Air.local>
2024-06-13 21:25:59 +0800

    updated server.js

--- server.js
+++ server.js
@@ -4,22 +4,21 @@ const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 8000
+const cors = require('cors');
 
 // Connect to database
 connectDB()
 
 const app = express()
-
+app.use(cors({
+    //change this origin shit every time i change my FE link
+    origin: ['http://localhost:3002'],
+    credentials: true
+  }));
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-// CORS middleware
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-  });
+
   
   app.get('/', (req, res) => {
     res.send('Hello');
